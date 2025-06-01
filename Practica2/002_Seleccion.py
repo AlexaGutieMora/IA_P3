df78763403f678abd1a17077c4f62ab369f63ec3
AlexaGutieMora <143145315+AlexaGutieMora@users.noreply.github.com>
2025-06-01 15:46:15 -0600

    Add files via upload

---
+++ Practica2/002_Seleccion.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jun  1 15:35:41 2025
+
+@author: k
+"""
+###################### SELECCIÓN
+def selection_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        min_idx = i
+        #Encuentra el mínimo restante
+        for j in range(i + 1, n):
+            if arr[j] < arr[min_idx]:
+                min_idx = j
+        #Intercambia el mínimo con el primero no ordenado
+        arr[i], arr[min_idx] = arr[min_idx], arr[i]
+    return arr
+
+print("Seleccion directa:", selection_sort([5, 2, 9, 1, 5, 6]))
